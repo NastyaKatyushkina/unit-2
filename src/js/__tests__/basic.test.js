25598afb4531dca676e2e720b0779cc512677421
NastyaKatyushkina <nastyusha_kat@mail.ru>
2022-10-20 12:24:07 +0500

    unit-2

---
+++ src/js/__tests__/basic.test.js
@@ -0,0 +1,30 @@
+import healthSort from '../health';
+
+test('healthSort', () => {
+  const data = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const dataSort = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+
+  expect(healthSort(data)).toEqual(dataSort);
+});
+test('healthSort1', () => {
+  const data = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+  const dataSort = [
+    { name: 'маг', health: 10 },
+    { name: 'лучник', health: 100 },
+    { name: 'мечник', health: 80 },
+  ];
+
+  expect(healthSort(data)).not.toBe(dataSort);
+});
